af3f77652e7caca9a739567355981cfbd77df5a9
sup39 <sup39@sup39.ml>
2021-01-06 00:20:12 +0900

    upload static

---
+++ app/out/_next/static/a4SnD4FFIRbAOqoWntHfn/pages/404.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{407:function(n,e,t){__NEXT_REGISTER_PAGE("/404",function(){return n.exports=t(408),{page:n.exports.default}})},408:function(n,e,t){"use strict";t.r(e);var u=t(1),o=t.n(u);e.default=function(){return o.a.createElement("div",null,"404")}}},[[407,1,0]]]);
